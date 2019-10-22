3acb9d4769f739952a9bbfe8c52476064659a9af
tinh980613 <ngovantinh0698@gmail.com>
2019-10-22 16:39:11 +0700

    update

--- module/Tinh-module/2/js/2.js
+++ module/Tinh-module/2/js/2.js
@@ -2,7 +2,10 @@ var swiper = new Swiper('.swiper-container', {
     speed: 600,
     parallax: true,
     loop: true,
-    
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
