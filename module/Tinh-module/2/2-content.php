4647cd90fbbf5641add8acdd77489bcd881b4a84
tinh980613 <ngovantinh0698@gmail.com>
2019-10-22 16:12:20 +0700

    update

--- module/Tinh-module/2/2-content.php
+++ module/Tinh-module/2/2-content.php
@@ -2,68 +2,106 @@
 $url_host = 'http://' . $_SERVER['HTTP_HOST'];
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-
 preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-2">
-    <div class="container">
+    <div class="container-fluid">
         <div class="swiper-container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-				<div class="parallax-bg" style="background-image:url(./images/main-rev-i-1.jpg)">
-			</div>
-				<div class="item-inner">
-                    <div class="title" data-swiper-parallax="-300">NEW JEWELRY</div>
-                    
-                    <div class="text" data-swiper-parallax="-100">
-                        <p>A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by Nillé</p>
-					</div>
-					<div class="disco">
-        				<div class="btn-discover">
-            				<div class="discover">
-                				<a href="#">Discover</a>
-            				</div>
-        				</div>
-    				</div>
-					</div>
+                    <div class="parallax-bg" style="background-image:url(./images/main-rev-i-1.jpg)">
+                    </div>
+                    <div class="item-inner">
+                        <div class="title" data-swiper-parallax="-300">NEW JEWELRY</div>
+                        
+                        <div class="text" data-swiper-parallax="-100">
+                            <p>A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by Nillé</p>
+                        </div>
+                        <div class="disco">
+                            <div class="btn-discover">
+                                <div class="discover">
+                                    <a href="#">Discover</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-after">
+                        <img src="./images/main-rev-image-5-1.png" alt="popup" class="img-fix">
+                        <img src="./images/main-rev-image-6.png" alt="popup" class="img-fix">
+                        <img src="./images/main-rev-image-7.png" alt="popup" class="img-fix">
+                    </div>
+                    <div class="item-before">
+                        <img src="./images/main-rev-image-1.png" alt="popup" class="img-fix1">
+                        <div class="item-before-inner">
+                            <h3>WHITE PEARL</h3>
+                            <p>Chagall Earrings</p>
+                            <span>$ 12,00</span>
+                        </div>
+                    </div>
                 </div>
                 <div class="swiper-slide">
-				<div class="parallax-bg" style="background-image:url(./images/main-rev-i-2.jpg)">
-			</div>
-			<div class="item-inner">
-                    <div class="title" data-swiper-parallax="-300" data-swiper-parallax-opacity="0">ALL DREAMLIKE</div>
-                    
-                    <div class="text" data-swiper-parallax="-100">
-                        <p>A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by Nillé</p>
-					</div>
-					<div class="disco">
-        				<div class="btn-discover">
-            				<div class="discover">
-                				<a href="#">Discover</a>
-            				</div>
-        				</div>
-    				</div>
-			</div>
+                    <div class="parallax-bg" style="background-image:url(./images/main-rev-i-2.jpg)">
+                    </div>
+                    <div class="item-inner">
+                        <div class="title" data-swiper-parallax="-300" data-swiper-parallax-opacity="0">ALL DREAMLIKE</div>
+                        
+                        <div class="text" data-swiper-parallax="-100">
+                            <p>A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by Nillé</p>
+                        </div>
+                        <div class="disco">
+                            <div class="btn-discover">
+                                <div class="discover">
+                                    <a href="#">Discover</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-after">
+                        <img src="./images/main-rev-image-5-1.png" alt="popup" class="img-fix">
+                        <img src="./images/main-rev-image-6.png" alt="popup" class="img-fix">
+                        <img src="./images/main-rev-image-7.png" alt="popup" class="img-fix">
+                    </div>
+                    <div class="item-before">
+                        <img src="./images/main-rev-image-8.png" alt="popup" class="img-fix1">
+                        <div class="item-before-inner">
+                            <h3>GOLDEN LOCKET</h3>
+                            <p>Sun Medallion</p>
+                            <span>$ 12,00</span>
+                        </div>
+                    </div>
                 </div>
                 <div class="swiper-slide">
-				<div class="parallax-bg" style="background-image:url(./images/main-rev-i-3.jpg)">
-			</div>
-			<div class="item-inner">
-                    <div class="title" data-swiper-parallax="-300">FRESH VISION</div>
-                    
-                    <div class="text" data-swiper-parallax="-100">
-                        <p>A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by Nillé</p>
-					</div>
-					<div class="disco">
-        				<div class="btn-discover">
-            				<div class="discover">
-                				<a href="#" class="dv">Discover</a>
-            				</div>
-        				</div>
-    				</div>
-			</div>
+                    <div class="parallax-bg" style="background-image:url(./images/main-rev-i-3.jpg)">
+                    </div>
+                    <div class="item-inner">
+                        <div class="title" data-swiper-parallax="-300">FRESH VISION</div>
+                        
+                        <div class="text" data-swiper-parallax="-100">
+                            <p>A breath of fresh air arrives to your jewelry box – browse our new collection, brought to you by Nillé</p>
+                        </div>
+                        <div class="disco">
+                            <div class="btn-discover">
+                                <div class="discover">
+                                    <a href="#" class="dv">Discover</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-after">
+                        <img src="./images/main-rev-image-5-1.png" alt="popup" class="img-fix">
+                        <img src="./images/main-rev-image-6.png" alt="popup" class="img-fix">
+                        <img src="./images/main-rev-image-7.png" alt="popup" class="img-fix">
+                    </div>
+                    <div class="item-before">
+                        <img src="./images/main-rev-image-9.png" alt="popup" class="img-fix1">
+                        <div class="item-before-inner">
+                            <h3>PLATED PENDANT</h3>
+                            <p>Braque Brooch</p>
+                            <span>$ 12,00</span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <!-- Add Pagination -->
--- module/Tinh-module/2/js/2.js
+++ module/Tinh-module/2/js/2.js
@@ -1,6 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
     speed: 600,
     parallax: true,
+    loop: true,
+    
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
