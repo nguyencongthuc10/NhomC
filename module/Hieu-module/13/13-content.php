077afcdfb229846f3e069eaefa9c673dc06a2ccf
nguyentrihieu0660 <nguyentrihieu0660@gmail.com>
2019-10-24 16:46:06 +0700

    24
    10

--- module/Hieu-module/13/13-content.php
+++ module/Hieu-module/13/13-content.php
@@ -6,38 +6,35 @@ preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-<div class="type-13">  
+<div class="type-13">
     <div class="container">
         <div class="row header-aboutus-13">
-                <div class="col-md-6 module-13-item">
-                    <div class="tittle-13">
-                        <h5 class="sub-13-aboutt">SUBSCRIBE TO NEWSLETTER</h5>
-                        <p class="abouus-13-oder" >Subscribe and get 10% off new collection</p>
-                    </div>
-                    
-                    <div role="form" class="abouts-md-13">
-                        <form>
-                            <div class="custom-email">
-                                <span class="wpcf7-form-control">
-                                    <input type="email" name="your-email" value="" size="40" class="input-13 wpcf7-text wpcf7-email" placeholder="Enter-mail">
-                                </span>
-                                <button type="submit" class="abouts-btn ">
-                                    <span class="qodef-btn-text">SEND</span>
-
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+            <div class="col-md-6 module-13-item">
+                <div class="tittle-13">
+                    <h5 class="sub-13-aboutt">SUBSCRIBE TO NEWSLETTER</h5>
+                    <p class="abouus-13-oder" >Subscribe and get 10% off new collection</p>
                 </div>
-                 <div class="col-md-6">
-                     <img src="images/aboutus-13.png" alt="" class="img-ab-13" >
-                       <div class="tittle-13">
-                        <h5 class="odr-13">ORDER YOUR GIFT CARD</h5>
-                        <p class="abouus-13-oder">Order your gift card today and get 8% off</p>
-                    </div>
-                 </div> 
-        </div> 
-
-
-</div>
+                
+                <div role="form" class="abouts-md-13">
+                    <form>
+                        <div class="custom-email">
+                            <span class="wpcf7-form-control">
+                                <input type="email" name="your-email" value="" size="40" class="input-13 wpcf7-text wpcf7-email" placeholder="Enter-mail">
+                            </span>
+                            <button type="submit" class="abouts-btn ">
+                            <span class="qodef-btn-text">SEND</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="images/aboutus-13.png" alt="" class="img-ab-13" >
+                <div class="tittle-13">
+                    <h5 class="odr-13">ORDER YOUR GIFT CARD</h5>
+                    <p class="abouus-13-oder">Order your gift card today and get 8% off</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- module/Hieu-module/17/17-content.php
+++ module/Hieu-module/17/17-content.php
@@ -6,30 +6,29 @@ preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-    <div class="type-17">
-        <!--CONTAINER-->
-        <div class="container line">
-            <!--ROW-->
-            <div class="row">
+<div class="type-17">
+    <!--CONTAINER-->
+    <div class="container line">
+        <!--ROW-->
+        <div class="row">
+            <div class="col-md-4">
+                <img src="images/Hieu-img-aboutus1.png" alt="" style="float: left;" >
+                <div class="name-ab-us"><h5>UNEVEN TEXTURE</h5>
+                    <p class="name-story">Our Story face feels organic</p></div>
+                    
+                </div>
                 <div class="col-md-4">
-                        <img src="images/Hieu-img-aboutus1.png" alt="" style="float: left;" >
+                    <img src="images/Hieu-img-aboutus2.png" alt="" style="float: left;">
+                    <div class="name-ab-us"><h5>UNEVEN TEXTURE</h5>
+                        <p class="name-story">Our Story face feels organic</p></div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="images/Hieu-imgaboutus3.png" alt="" style="float: left; display: block;">
                         <div class="name-ab-us"><h5>UNEVEN TEXTURE</h5>
-                        <p class="name-story">Our Story face feels organic</p></div>
-                        
+                            <p class="name-story">Our Story face feels organic</p></div>
+                        </div>
+                    </div>
+                    <!--/ROW-->
                 </div>
-                <div class="col-md-4">
-                     <img src="images/Hieu-img-aboutus2.png" alt="" style="float: left;">
-                      <div class="name-ab-us"><h5>UNEVEN TEXTURE</h5>
-                        <p class="name-story">Our Story face feels organic</p></div>
-                </div>
-                <div class="col-md-4">
-                     <img src="images/Hieu-imgaboutus3.png" alt="" style="float: left; display: block;">
-                      <div class="name-ab-us"><h5>UNEVEN TEXTURE</h5>
-                        <p class="name-story">Our Story face feels organic</p></div>
-                </div>
+                <!--/CONTAINER-->
             </div>
-            <!--/ROW-->
-        </div>
-        <!--/CONTAINER-->
-    </div>
-
